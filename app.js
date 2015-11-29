cc2555ad4b47f5208bbc00c1b24ce8362c03911f
Heber Samuel Lee <hsl2113@columbia.edu>
2015-11-29 14:55:38 -0500

    small mods to app.js

--- app.js
+++ app.js
@@ -1,30 +1,33 @@
-var express = require('express');
+var express = require('express')
 var app = express();
-var router = express.Router();
+var server = require('http').Server(app);
+//var router = app.Router();
 var path = __dirname + '/public/';
+var io = require('socket.io')(server)
 
 app.use(express.static('public'));
 app.use(express.static('../../node_modules/bootstrap/'));
 
-router.use(function (req, res, next) {
-	console.log('/' + req.method);
-	next();
-});
+// router.use(function (req, res, next) {
+// 	console.log('/' + req.method);
+// 	next();
+// });
+server.listen(8080);
+console.log("listening on Port: 8080");
 
-
-router.get('/', function(req, res) {
+app.get('/', function(req, res) {
 	res.sendFile(path + 'index.html');
 });
 
 
 
-app.use('/', router);
+//app.use('/', router);
 
-app.use('*', function(req,res) {
-	res.sendFile(path + '404.html');
-});
+// app.use('*', function(req,res) {
+// 	res.sendFile(path + '404.html');
+// });
 
 
-app.listen(8080, function() {
-	console.log("listening on Port: 8080");
-});
+// app.listen(8080, function() {
+// 	console.log("listening on Port: 8080");
+// });
