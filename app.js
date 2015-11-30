e59f1ed0ba127e53647ba990320701aaed20bd35
aa920502 <davidlucs@gmail.com>
2015-11-29 19:13:09 -0500

    enable search by category

--- app.js
+++ app.js
@@ -7,11 +7,42 @@ var io = require('socket.io')(server)
 
 var query = require('./queryDynamoDB.js');
 
+
 app.use(express.static('public'));
 app.use(express.static('socket.io'));
 app.use(express.static('../../node_modules/bootstrap/'));
 
 DEBUG='socket.io node app.js'
+
+
+
+server.listen(8080);
+console.log("listening on Port: 8080");
+
+app.get('/', function(req, res) {
+    res.sendFile(path + 'index.html');
+});
+
+io.on('connection', function(socket) {
+    socket.emit('news', {hello: 'world' });
+    socket.on('other', function (data) {
+        console.log(data.ans);
+        Search(data.ans);        
+    });
+});
+
+// ****************************************************************************
+// Scan through dynamo db and retrieve tweets which contains 'keyword' in text
+// ****************************************************************************
+
+var AWS = require("aws-sdk");
+
+AWS.config.update({
+  region: "us-east-1",
+});
+
+var dynamodbDoc = new AWS.DynamoDB.DocumentClient();
+
 
 var keywords = new Array();
 keywords['Cities'] = new Array('new york', 'paris', 'los angeles', 'london', 'tokyo', 'rome', 'shanghai');
@@ -20,45 +51,66 @@ keywords['Holidays'] = new Array('thanksgiving', 'christmas', 'new year', 'xmas'
 keywords['Politics'] = new Array('obama', 'clinton', 'trump', 'republican', 'democrat', 'rubio', 'bush', 'sanders', 'bernie', 'carson', 'cruz');
 keywords['Twitter Trending'] = 'top trends twitter api results';
 
-//console.log(keywords['currently trending']);
+var params = {
+    TableName : "Tweets",
+    ProjectionExpression: "#tweet_id, #t, #s, #c",
+    FilterExpression: "#tweet_id > :tweet_id",  // retrieve tweets which has tweet_id greater than 0 (all tweets)
+    ExpressionAttributeNames:{
+        "#tweet_id": "tweet_id",
+        "#t": "text",
+        "#s": "sentiment",
+        "#c": "coordinates"
+    },
+    ExpressionAttributeValues: {
+        ":tweet_id": "000000000000000000"
+    }
+};
 
-function searchDB(keyword){
-	if (keyword !== 'void' ){
-		for (var i = 0; i < keywords[keyword].length; i++) {
-			query.queryDynamoDB(keywords[keyword][i]);
-		}
-	}
+var term = "";
+
+function Search(keyword){
+    var arr = new Array();
+    arr['sentiment'] = new Array();
+    arr['coordinates'] = new Array();
+
+    if (keyword !== 'void' ){
+        console.log('*********************************');
+        console.log("Querying in the table for category" + keyword);
+        console.log('*********************************');
+        dynamodbDoc.scan(params, function(err, data) {
+            if (err) {
+                console.error("Unable to query. Error:", JSON.stringify(err, null, 2));
+            } else {
+                console.log("Query succeeded.");
+                data.Items.forEach(function(item) {
+                    // console.log(item.text.toLowerCase());
+                    for (var i = 0; i < keywords[keyword].length; i++) {
+                        term = keywords[keyword][i];
+                        if(item.text.toLowerCase().indexOf(term) > -1 ){
+                            // console.log("keyword found");
+                            // console.log("Tweet_ID: ", item.tweet_id + " - text: " + item.text + " - sentiment " + item.sentiment + " - coordinates" + item.coordinates);
+                            if(!item.sentiment){
+                                console.log('0');
+                                arr['sentiment'].push("0");
+                                arr['coordinates'].push(item.coordinates);
+                            }
+                            else{
+                                console.log('1');
+                                arr['sentiment'].push(item.sentiment);
+                                arr['coordinates'].push(item.coordinates);
+                            }
+                        }
+                        else{
+                            //console.log("keyword not found")
+                        }
+                    }
+                    
+                });
+            }
+            console.log(arr);
+        });
+        
+    }
+    
 }
-//query.queryDynamoDB(keywords['holidays'][0]);
 
-//searchDB('politics');
-
-// router.use(function (req, res, next) {
-// 	console.log('/' + req.method);
-// 	next();
-// });
-server.listen(8080);
-console.log("listening on Port: 8080");
-
-app.get('/', function(req, res) {
-	res.sendFile(path + 'index.html');
-});
-
-io.on('connection', function(socket) {
-	socket.emit('news', {hello: 'world' });
-	socket.on('other', function (data) {
-		console.log(data.ans);
-		searchDB(data.ans);
-	});
-});
-
-//app.use('/', router);
-
-// app.use('*', function(req,res) {
-// 	res.sendFile(path + '404.html');
-// });
-
-
-// app.listen(8080, function() {
-// 	console.log("listening on Port: 8080");
-// });
--- queryDynamoDB.js
+++ queryDynamoDB.js
@@ -9,15 +9,22 @@ AWS.config.update({
 var dynamodbDoc = new AWS.DynamoDB.DocumentClient();
 
 
-
+var keywords = new Array();
+keywords['Cities'] = new Array('new york', 'paris', 'los angeles', 'london', 'tokyo', 'rome', 'shanghai');
+keywords['Sports'] = new Array('soccer', 'football', 'basketball', 'baseball', 'tennis', 'golf', 'giants', 'patriots', 'barcelona', 'real madrid');
+keywords['Holidays'] = new Array('thanksgiving', 'christmas', 'new year', 'xmas');
+keywords['Politics'] = new Array('obama', 'clinton', 'trump', 'republican', 'democrat', 'rubio', 'bush', 'sanders', 'bernie', 'carson', 'cruz');
+keywords['Twitter Trending'] = 'top trends twitter api results';
 
 var params = {
     TableName : "Tweets",
-    ProjectionExpression: "#tweet_id, #t",
+    ProjectionExpression: "#tweet_id, #t, #s, #c",
     FilterExpression: "#tweet_id > :tweet_id",  // retrieve tweets which has tweet_id greater than 0 (all tweets)
     ExpressionAttributeNames:{
         "#tweet_id": "tweet_id",
-        "#t": "text"
+        "#t": "text",
+        "#s": "sentiment",
+        "#c": "coordinates"
     },
     ExpressionAttributeValues: {
         ":tweet_id": "000000000000000000"
@@ -26,25 +33,54 @@ var params = {
 
 var query = exports;
 
-query.queryDynamoDB = function(term) {
-    console.log('*********************************');
-    console.log("Querying in the table for ", term);
-    console.log('*********************************');
-    dynamodbDoc.scan(params, function(err, data) {
-        if (err) {
-            console.error("Unable to query. Error:", JSON.stringify(err, null, 2));
-        } else {
-            console.log("Query succeeded.");
-            data.Items.forEach(function(item) {
 
-                if(item.text.toLowerCase().indexOf(term) > -1 ){
-                    console.log("keyword found");
-                    console.log("Tweet_ID: ", item.tweet_id + " - text: " + item.text);
-                }
-                else{
-                    // console.log("keyword not found")
-                }
-            });
-        }
-    });
+
+
+var term = "";
+    
+query.queryDynamoDB = function (keyword){
+    var arr = new Array();
+    arr['sentiment'] = new Array();
+    arr['coordinates'] = new Array();
+
+    if (keyword !== 'void' ){
+        console.log('*********************************');
+        console.log("Querying in the table for category" + keyword);
+        console.log('*********************************');
+        dynamodbDoc.scan(params, function(err, data) {
+            if (err) {
+                console.error("Unable to query. Error:", JSON.stringify(err, null, 2));
+            } else {
+                console.log("Query succeeded.");
+                data.Items.forEach(function(item) {
+                    // console.log(item.text.toLowerCase());
+                    for (var i = 0; i < keywords[keyword].length; i++) {
+                        term = keywords[keyword][i];
+                        if(item.text.toLowerCase().indexOf(term) > -1 ){
+                            // console.log("keyword found");
+                            // console.log("Tweet_ID: ", item.tweet_id + " - text: " + item.text + " - sentiment " + item.sentiment + " - coordinates" + item.coordinates);
+                            if(!item.sentiment){
+                                console.log('0');
+                                arr['sentiment'].push("0");
+                                arr['coordinates'].push(item.coordinates);
+                            }
+                            else{
+                                console.log('1');
+                                arr['sentiment'].push(item.sentiment);
+                                arr['coordinates'].push(item.coordinates);
+                            }
+                        }
+                        else{
+                            //console.log("keyword not found")
+                        }
+                    }
+                    
+                });
+            }
+            console.log(arr);
+        });
+        
+    }
+    
 }
+
