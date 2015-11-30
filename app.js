509db703aabc0f635f8152f3074e7fd13d845061
aa920502 <davidlucs@gmail.com>
2015-11-30 10:31:10 -0500

    able to display sentiment on server side

--- app.js
+++ app.js
@@ -1,14 +1,9 @@
 var express = require('express')
 var app = express();
 var server = require('http').Server(app);
-//var router = app.Router();
 var path = __dirname + '/public/';
 var io = require('socket.io')(server)
 var getTweets = require('./getAndStoreTweets.js');
-
-
-// var query = require('./queryDynamoDB.js');
-
 
 app.use(express.static('public'));
 app.use(express.static('socket.io'));
@@ -17,11 +12,8 @@ app.use(express.static('../../node_modules/bootstrap/'));
 DEBUG='socket.io node app.js'
 
 
-
 server.listen(8080);
 console.log("listening on Port: 8080");
-
-
 
 app.get('/', function(req, res) {
     res.sendFile(path + 'index.html');
@@ -31,11 +23,12 @@ io.on('connection', function(socket) {
     socket.emit('news', {hello: 'world' });
     socket.on('selection', function (data) {
         console.log(data.ans);
-        Search(data.ans, socket);
-        getTweets.getAndStore(keywords[data.ans]);
-        //socket.emit('map', )        
+        Search(data.ans, socket); //Search for chosen category
+        //getTweets.getAndStore(keywords[data.ans]);
     });
 });
+
+
 
 // ****************************************************************************
 // Scan through dynamo db and retrieve tweets which contains 'keyword' in text
@@ -72,16 +65,20 @@ var params = {
     }
 };
 
-var term = "";
 
 function Search(keyword, socket){
     var arr = new Array();
     arr['sentiment'] = new Array();
     arr['coordinates'] = new Array();
+    // term to search for
+    var term = "";
+    // sentiment variable
+    var sentiment = 0;
+    
 
     if (keyword !== 'void' ){
         console.log('*********************************');
-        console.log("Querying in the table for category" + keyword);
+        console.log("Querying in the table for category: " + keyword);
         console.log('*********************************');
         dynamodbDoc.scan(params, function(err, data) {
             if (err) {
@@ -100,6 +97,7 @@ function Search(keyword, socket){
                                 arr['coordinates'].push(item.coordinates);
                             }
                             else{
+                            	sentiment += parseFloat(item.sentiment);
                                 arr['sentiment'].push(item.sentiment);
                                 arr['coordinates'].push(item.coordinates);
                             }
@@ -112,6 +110,7 @@ function Search(keyword, socket){
                 });
             }
             //console.log(arr['coordinates']);
+            console.log("current sentiment is: " + sentiment);
             socket.emit('map', arr['coordinates']);
         });
         
